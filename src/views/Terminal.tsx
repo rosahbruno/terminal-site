e87ad3a44c58f9262ddf80577deaa1b8d815188a
Bruno Rosa <rosahbruno@gmail.com>
2021-04-06 15:13:18 -0400

    refactor: remove unnecessary import

--- src/views/Terminal.tsx
+++ src/views/Terminal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import { FC, useState } from 'react';
 
 // *** context *** //
 import { ThemeContext, ThemeOptions, themes } from '../context/themeContext';
